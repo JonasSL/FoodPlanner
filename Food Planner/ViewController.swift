2d042b746fe158d9770948803290e73aa286e432
JonasSL <jonaslarsen1994@gmail.com>
2015-11-21 16:32:07 +0100

    hejhej

--- Food Planner/ViewController.swift
+++ Food Planner/ViewController.swift
@@ -17,7 +17,7 @@ class ViewController: UIViewController {
     
     var DB = [Product]()
     var knownDishes = [Dish]()
-    
+    //test
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
